c6bd1a409c52db0de0958efaa81a757d7ba4c7ca
hhs333 <163867431+hhs333@users.noreply.github.com>
2024-12-14 16:15:11 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>UOB Students Nationality Data</title> 
+
+ 
+    <link rel="stylesheet" href="index.css">
+    
+
+    <link rel="stylesheet" href="https://unpkg.com/pico-css">
+</head>
+
+<body>
+    <main>
+  
+        <h1 class="container">University of Bahrain Nationality Data</h1>
+
+  
+        <table>
+            <thead>
+                <tr>
+                
+                    <th>Year</th>
+                    <th>Semester</th>
+                    <th>The Programs</th>
+                    <th>Nationality</th>
+                    <th>Colleges</th>
+                    <th>Number of Students</th>
+                </tr>
+            </thead> 
+            <tbody id="table-body">
+         
+            </tbody>
+        </table>
+    </main>
+
+    <script src="script.js"></script>
+</body>
+</html>
